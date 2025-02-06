db92f7f465c4f2daabc3eda251d881e5fd1a2de4
Richard <richardlwhittington@gmail.com>
2025-02-06 13:46:58 -0600

    add prep acc

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,11 @@
 // Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
 
+//parameters 
+//returns
+//examples
+//pseudocode
+
+
 function fizzBuzz(num){
     //interate over 1 to num
     //checks if int is divisble by 3 or 5
